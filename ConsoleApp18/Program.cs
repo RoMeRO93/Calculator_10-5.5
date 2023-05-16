44270fbd3497730a7745970708d6259361bbea90
RoMeRO93 <122970841+RoMeRO93@users.noreply.github.com>
2023-05-16 15:32:15 +0300

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,24 @@
+﻿NewMethod();
+
+static void NewMethod()
+{
+    int numar1;
+   double numar2;
+    double rezultat;
+    double rest;
+        numar1 = 10;
+        numar2 = 5.5;
+    Console.WriteLine(numar1 + numar2);
+    Console.WriteLine(numar1 - numar2);
+    Console.WriteLine(numar1 * numar2);
+    Console.WriteLine(numar1 / numar2);
+    Console.WriteLine(numar1 + numar2);
+
+    rezultat = numar1 + numar2 * 2 / 3;
+    rest = 10 % 4;
+    Console.WriteLine(rezultat);
+    Console.WriteLine(rest);
+
+    Console.WriteLine("Terminat");
+}
+
